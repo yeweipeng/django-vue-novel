57085285b38c9e5a0aebf083e2ac8e7e9ac60311
ywp <yedezhanghao@gmail.com>
2018-06-03 00:16:17 +0800

    add chapter API and content API

--- django_vue_novel/urls.py
+++ django_vue_novel/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^api/', include("api.urls")),
+    url(r'^book/api/', include("api.urls")),
     url(r'^admin/', admin.site.urls),
 ]
